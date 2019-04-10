6741092480a56f9beaec47b6cc7bbc8106fe3767
Ricardo3100 <38477282+Ricardo3100@users.noreply.github.com>
2019-04-10 07:44:50 -0700

    first commit

---
+++ app/src/main/java/com/example/ricardo/blindacccessibility/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.ricardo.blindacccessibility;
+
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.SwitchCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+
+import com.example.ricardo.blindacccessibility.InitApplication;
+import com.example.ricardo.blindacccessibility.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (InitApplication.getInstance().isNightModeEnabled()) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+        setContentView(R.layout.activity_main);
+
+
+        SwitchCompat switchCompat = findViewById(R.id.switchCompat);
+
+
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES)
+            switchCompat.setChecked(true);
+
+        switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    InitApplication.getInstance().setIsNightModeEnabled(true);
+                    Intent intent = getIntent();
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    finish();
+                    startActivity(intent);
+
+                } else {
+                    InitApplication.getInstance().setIsNightModeEnabled(false);
+                    Intent intent = getIntent();
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    finish();
+                    startActivity(intent);
+                }
+
+
+            }
+        });
+
+    }
+}
